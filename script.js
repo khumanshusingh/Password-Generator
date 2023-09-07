22cfd79180433c01ca9d043d9b8dbf17dc981e97
khumanshusingh <khumanshusingh@gmail.com>
2023-09-08 03:27:15 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,112 @@
+let passwordEl1 = document.getElementById("password-el1");
+let passwordEl2 = document.getElementById("password-el2");
+let buttonEl = document.getElementById("button-el");
+const characters = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+  "0",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "~",
+  "`",
+  "!",
+  "@",
+  "#",
+  "$",
+  "%",
+  "^",
+  "&",
+  "*",
+  "(",
+  ")",
+  "_",
+  "-",
+  "+",
+  "=",
+  "{",
+  "[",
+  "}",
+  "]",
+  ",",
+  "|",
+  ":",
+  ";",
+  "<",
+  ">",
+  ".",
+  "?",
+  "/",
+];
+let passwordOne = 0;
+let passwordTwo = 0;
+function generatePassword() {
+  passwordEl1.textContent = " ";
+  passwordEl2.textContent = " ";
+  let passwordOne = 0;
+  let passwordTwo = 0;
+  for (let i = 0; i < 15; i++) {
+    let randomIndexOne = Math.floor(Math.random() * characters.length);
+    let randomIndexTwo = Math.floor(Math.random() * characters.length);
+    passwordOne += characters[randomIndexOne];
+    passwordTwo += characters[randomIndexTwo];
+  }
+  passwordEl1.textContent = passwordOne;
+  passwordEl2.textContent = passwordTwo;
+}
